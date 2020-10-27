ad6c5d6c210910e6ba5a4b30a89ff34781874590
Christian Go <cvgo2000@yahoo.com>
2020-10-26 20:26:11 -0600

    Adjustments to root files

--- script.js
+++ script.js
@@ -1,180 +1,99 @@
-// Moment declaration for day and time 
-moment(Date);
-$("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
-// Moment Current time 
-var currentTime = moment();
-// Returns current time to the nearest hour - 12:30 becomes 12:00
-currentTime = currentTime.startOf("hour");
-// Calculates the start of day + 9 to return 9 am
-var beforeTime = moment().startOf('day').add(9, "hours");
+$(document).ready(function() {
+    // Sets text content of HTML element with id of currentDay to today's date using moment.js
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
-// Time blocks
-// 9 AM 
-var time1 = beforeTime.add(0, "h");
-// var time1 = beforeTime;
-time1 = time1.format('hh:mm A');
-$(".block1").text(time1);
-// 10 AM
-var time2 = beforeTime.add(1, "h");
-time2 = time2.format('hh:mm A');
-$(".block2").text(time2);
-// 11 AM
-var time3 = beforeTime.add(1, "h");
-time3 = time3.format('hh:mm A');
-$(".block3").text(time3);
-// 12 PM
-var time4 = beforeTime.add(1, "h");
-time4 = time4.format('hh:mm A');
-$(".block4").text(time4);
-// 1 PM
-var time5 = beforeTime.add(1, "h");
-time5 = time5.format('hh:mm A');
-$(".block5").text(time5);
-// 2 PM
-var time6 = beforeTime.add(1, "h");
-time6 = time6.format('hh:mm A');
-$(".block6").text(time6);
-// 3 PM
-var time7 = beforeTime.add(1, "h");
-time7 = time7.format('hh:mm A');
-$(".block7").text(time7);
-// 4 PM
-var time8 = beforeTime.add(1, "h");
-time8 = time8.format('hh:mm A');
-$(".block8").text(time8);
-// 5 PM
-var time9 = beforeTime.add(1, "h");
-time9 = time9.format('hh:mm A');
-$(".block9").text(time9);
+    // Array for time - used to dynamically create time blocks on the page
+    const timeArr = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
 
-// Function to compare hour slots with current time 
-function testTime() {
-    // Add time1 9AM
-    time1 = moment().startOf('day').add(9, "hours");
-    // Adjusts current time to the hour
-    currentTime = currentTime.startOf("hour");
-    // Add time1 if/else
-    if (currentTime.isAfter(time1)) {
-        $(".form9").addClass("past");
-    }
-    else if (currentTime.isBefore(time1)) {
-        $(".form9").addClass("future");
-    }
-    else if (currentTime.isSame(time1)) {
-        $(".form9").addClass("present");
-    };
-    // Add time2 10AM
-    time2 = moment().startOf('day').add(10, "hours");
-    // Add time2 if/else
-    if (currentTime.isAfter(time2)) {
-        $(".form10").addClass("past");
-    }
-    else if (currentTime.isBefore(time2)) {
-        $(".form10").addClass("future");
-    }
-    else if (currentTime.isSame(time2)) {
-        $(".form10").addClass("present");
-    };
-    // time3 11AM
-    time3 = moment().startOf('day').add(11, "hours");
-    // Add time3 if/else
-    if (currentTime.isAfter(time3)) {
-        $(".form11").addClass("past");
-    }
-    else if (currentTime.isBefore(time3)) {
-        $(".form11").addClass("future");
-    }
-    else if (currentTime.isSame(time3)) {
-        $(".form11").addClass("present");
-    };
-    // time4 12PM
-    time4 = moment().startOf('day').add(12, "hours");
-    // Add time4 if/else
-    if (currentTime.isAfter(time4)) {
-        $(".form12").addClass("past");
-    }
-    else if (currentTime.isBefore(time4)) {
-        $(".form12").addClass("future");
-    }
-    else if (currentTime.isSame(time4)) {
-        $(".form12").addClass("present");
-    };
-    // time5 1PM
-    time5 = moment().startOf('day').add(13, "hours");
-    // Add time5 if/else
-    if (currentTime.isAfter(time5)) {
-        $(".form1").addClass("past");
-    }
-    else if (currentTime.isBefore(time5)) {
-        $(".form1").addClass("future");
-    }
-    else if (currentTime.isSame(time5)) {
-        $(".form1").addClass("present");
-    };
-    // time6 2PM
-    time6 = moment().startOf('day').add(14, "hours");
-    // Add time6 if/else
-    if (currentTime.isAfter(time6)) {
-        $(".form2").addClass("past");
-    }
-    else if (currentTime.isBefore(time6)) {
-        $(".form2").addClass("future");
-    }
-    else if (currentTime.isSame(time6)) {
-        $(".form2").addClass("present");
-    };
-    // time7 3M
-    time7 = moment().startOf('day').add(15, "hours");
-    // Add time7 if/else
-    if (currentTime.isAfter(time7)) {
-        $(".form3").addClass("past");
-    }
-    else if (currentTime.isBefore(time7)) {
-        $(".form3").addClass("future");
-    }
-    else if (currentTime.isSame(time7)) {
-        $(".form3").addClass("present");
-    };
-    // time8 4pm
-    time8 = moment().startOf('day').add(16, "hours");
-    // Add time8 if/else
-    if (currentTime.isAfter(time8)) {
-        $(".form4").addClass("past");
-    }
-    else if (currentTime.isBefore(time8)) {
-        $(".form4").addClass("future");
-    }
-    else if (currentTime.isSame(time8)) {
-        $(".form4").addClass("present");
-    };
-    // time9 5pm
-    time9 = moment().startOf('day').add(17, "hours");
-    // Add time9 if/else
-    if (currentTime.isAfter(time9)) {
-        $(".form5").addClass("past");
-    }
-    else if (currentTime.isBefore(time9)) {
-        $(".form5").addClass("future");
-    }
-    else if (currentTime.isSame(time9)) {
-        $(".form5").addClass("present");
-    };
-}
-testTime();
-// Loops through input area to get item from local storage
-var x = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+    // Assigning container div to variable, to grab and append to at the end of loop below
+    var mainDisplay = $("#displayContent");
 
+    // Creating an empty array to store user input for todos
+    var todoList = [];
 
-for (var i = 0; i < x.length; i++) {
-    var dataHour = localStorage.getItem(x[i]);
-    $(".form" + x[i]).val(dataHour);
-}
-// Event listener to save to local storage
-$(".saveBtn").click(function () {
-    event.preventDefault();
-    var formValue = $(this).siblings(".form-control").val();
-    console.log("This worked");
-    var listItem = $(this).parent().data("hour");
+    // Loops through timeArr to dynamically create display
+    timeArr.forEach((element, index) => {
+        // ROW: create div row containing each time block
+        var newRow = $("<div>").addClass("row");
 
-    localStorage.setItem(listItem, formValue);
+        // COLUMN 1: create column div for first column with time (2 blocks)
+        var col1 = $("<div>").addClass("col-sm-2 time-block hour");
+            // sets text to AM or PM based on array element/time
+            if(element <= 11) {
+                col1.text(`${element}AM`);
+            }
+            else if (element == 12){
+                col1.text(`${element}PM`);
+            }
+            else {
+                col1.text(`${element-12}PM`);
+            }
+
+        // COLUMN 2: create column div for second column with text area (9 blocks)
+        var col2 = $("<textarea>").addClass("col-sm-9 row text-column");
+        col2.attr("id", "col-text-"+element);
+            // checks current hour and sets column class to past, present, or future based on the time
+            if(element == moment().hour()) {
+                col2.addClass("present");
+            }
+            else if(element < moment().hour()) {
+                col2.addClass("past");
+            }
+            else {
+                col2.addClass("future");
+            }
+
+        // COLUMN 3: create column div for third column with save button (1 block)
+        var col3 = $("<button type='submit'>").addClass("col-sm-1 saveBtn");
+        col3.attr("value", element);
+        col3.append($("<i class='fas fa-save'>"));
+
+        // appending newly created columns to newly created row, and then appending row to container div
+        newRow.append(col1);
+        newRow.append(col2);
+        newRow.append(col3);
+        mainDisplay.append(newRow);
+    });
+
+    // Adding event listener to save buttons
+    $("button").on("click", saveToLocalStorage);
+
+    // This is called whenever a button is clicked, storing the value of user inputs to local storage
+    function saveToLocalStorage() {
+        var todoObject = {
+            hour: $(this).val(),
+            todo: $("#col-text-"+$(this).val()).val()
+        }
+        // pushes newly created todoObject to todoList array, then sets the todoList array to localStorage
+            todoList.push(todoObject);
+            localStorage.setItem("todoList", JSON.stringify(todoList));
+    }
+
+    // If local storage contains data, this retrieves the data and displays it
+    function renderToDoList() {
+        var storedTodos = JSON.parse(localStorage.getItem("todoList"));
+        if(storedTodos !== null) {
+            todoList = storedTodos;
+
+            todoList.forEach((element, index) => {
+                $("#col-text-"+(todoList[index].hour)).text(todoList[index].todo);
+            });
+        }
+    }
+
+    renderToDoList();
+
+    // Event listener for triple click, will ask user if they want to delete todo
+    $(".text-column").on('click', function(event) {
+        if (event.detail === 3) {
+            if(confirm("Are you sure you want to delete this item?")) {
+                var columnToDelete = $(this).attr("id");
+                todoList = $.grep(todoList, function(e) {
+                    return e.hour != columnToDelete.slice(9);
+                })
+                localStorage.setItem("todoList", JSON.stringify(todoList));
+                $(this).val("");
+            }
+        }
+    });
 });
